ccd80a2949a928d6b1906fdb87a785ad6eaaa9e2
Victor Haine <victor.haine@icloud.com>
2024-08-16 22:50:30 -0300

    feat: add mouse edge start and end event

--- src/plugins/MouseEdges.ts
+++ src/plugins/MouseEdges.ts
@@ -1,7 +1,7 @@
 import { Plugin } from './Plugin';
 
-import type { Viewport } from '../Viewport';
 import type { InteractionEvent } from '@pixi/interaction';
+import type { Viewport } from '../Viewport';
 
 /** Insets for mouse edges scrolling regions */
 export interface IMouseEdgesInsets
@@ -158,6 +158,8 @@ export class MouseEdges extends Plugin
         const x = event.data.global.x;
         const y = event.data.global.y;
 
+        const wasScrolling = this.horizontal !== 0 || this.vertical !== 0;
+
         if (this.radiusSquared)
         {
             const center = this.parent.toScreen(this.parent.center);
@@ -220,6 +222,23 @@ export class MouseEdges extends Plugin
                 this.decelerateVertical();
                 this.vertical = 0;
             }
+        }
+
+        const isScrolling = this.horizontal !== 0 || this.vertical !== 0;
+
+        if (!wasScrolling && isScrolling)
+        {
+            this.parent.emit('mouse-edge-start', {
+                viewport: this.parent,
+                type: 'mouse-edges',
+            });
+        }
+        else if (wasScrolling && !isScrolling)
+        {
+            this.parent.emit('mouse-edge-end', {
+                viewport: this.parent,
+                type: 'mouse-edges',
+            });
         }
 
         return false;
